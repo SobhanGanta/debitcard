f30d7b8418ab36265d8f8e8af517373315c8aa77
SobhanGanta <sobhan.ganta@gmail.com>
2016-10-21 18:32:36 +0530

    initial commit

---
+++ DebitCard/src/main/java/com/cts/dev/Transation.java
@@ -0,0 +1,10 @@
+package com.cts.dev;
+
+public class Transation {
+	public void insert() {
+		System.out.println("HEllo transation");
+	}
+	public static void main(String[] args) {
+		System.out.println("hai"+"Anji");
+	}
+}
