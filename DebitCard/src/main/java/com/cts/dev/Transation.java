5bdbe664625b0518c89243a1590a58b665cc6a8f
SobhanGanta <sobhan.ganta@gmail.com>
2016-10-21 18:41:54 +0530

    koti change1

--- DebitCard/src/main/java/com/cts/dev/Transation.java
+++ DebitCard/src/main/java/com/cts/dev/Transation.java
@@ -1,10 +1,8 @@
 package com.cts.dev;
 
 public class Transation {
-	public void insert() {
-		System.out.println("HEllo transation");
-	}
+
 	public static void main(String[] args) {
-		System.out.println("hai"+"Anji");
+		System.out.println("hai" + "Anji" + "Koti"+"Sobhan");
 	}
 }
