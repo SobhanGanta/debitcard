e6d4ddba647047c65c973dd5cddf58383e1879b8
koti <kkr.kothapally@gmmail.com>
2016-10-23 18:55:15 +0530

    this is the 2nd commit by koti-CMD

--- DebitCard/src/main/java/com/cts/dev/Transation.java
+++ DebitCard/src/main/java/com/cts/dev/Transation.java
@@ -6,5 +6,6 @@ public class Transation {
 System.out.println("this is the change from command prompt");
 
 System.out.println("hai" + "Anji" + "KotiBabu"+"SobhanbABU"+"Varma");
+System.out.println("Hello this is koti from Vadapalani has Changed the code by 23/10/2016");
 	}
 }
