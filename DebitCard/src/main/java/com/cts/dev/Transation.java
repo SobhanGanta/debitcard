f2ac6af7e177583489418442945a0d019c3fea27
SobhanGanta <sobhan.ganta@gmail.com>
2016-10-23 18:46:35 +0530

    this  is  the commit by Koti-cmd

--- DebitCard/src/main/java/com/cts/dev/Transation.java
+++ DebitCard/src/main/java/com/cts/dev/Transation.java
@@ -3,6 +3,8 @@ package com.cts.dev;
 public class Transation {
 
 	public static void main(String[] args) {
-		System.out.println("hai" + "Anji" + "KotiBabu"+"SobhanbABU"+"Varma");
+System.out.println("this is the change from command prompt");
+
+System.out.println("hai" + "Anji" + "KotiBabu"+"SobhanbABU"+"Varma");
 	}
 }
